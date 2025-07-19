58f54fab750710826372de33b8091e5058a13759
rafal1137 <rafal1137@yahoo.pl>
2025-07-19 23:08:51 +0200

    Update ETLBuildClient.cmake

--- cmake/ETLBuildClient.cmake
+++ cmake/ETLBuildClient.cmake
@@ -56,7 +56,7 @@ if(EMSCRIPTEN)
 	  "-s ENVIRONMENT=web,worker,node"
 	  "-s USE_PTHREADS=0"
 	  "-s GL_UNSAFE_OPTS=0"
-	  "-s EXIT_RUNTIME=1"
+	  "-s NO_EXIT_RUNTIME=1"
 	)
 
     string(REPLACE ";" " " EMSCRIPTEN_LINKER_FLAGS_STR "${EMSCRIPTEN_LINKER_FLAGS}")
